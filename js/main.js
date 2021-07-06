8e6ed42c150269a781613ea7d55ad9321a32881c
Daniel Buczko <d.buczko@o2.pl>
2021-07-06 19:40:31 +0200

    Rozwiązanie zadania

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 9;
+let secondNumber = 31;
+
+function myFunction(a, b) {
+  let result = a + b;
+  return result;
+}
+
+let functionResult = myFunction(firstNumber, secondNumber);
+console.log(functionResult);
